f7b8a8c7d55ebf42ce4df1f6292962bf42c46de1
fancy zero <fancyzero@live.com>
2022-01-14 01:46:54 +0800

    initial

---
+++ Assets/EyeBallGen.cs
@@ -0,0 +1,142 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyeBallGen : MonoBehaviour
+{
+    [Range(1,100)]
+    public   float verticesDensity = 1;
+	[Range(1,10)]
+	public int smoothOrder;	
+	[Range(0,0.5f)]
+	public float bump;
+	[Range(0,1.5f)]
+	public float smooth;
+	[Range(0.1f,2.0f)]
+	public float eyeballRadius;
+	float smoothstep(float edge0, float edge1, float x) {
+	// Scale, bias and saturate x to 0..1 range
+	//x = Mathf.Clamp((x - edge0) / (edge1 - edge0), 0.0f, 1.0f); 
+	// Evaluate polynomial
+
+	return generalSmoothStep( smoothOrder, x);
+	//return x * x * (3 - 2 * x);
+	}
+	float GetRadius(float r, float angle)
+	{
+		float fix = 1.0f- generalSmoothStep(smoothOrder,Mathf.Clamp(angle/smooth, 0.0f, 1.0f));
+		return r+fix*bump;
+
+		
+	}
+float generalSmoothStep(int a, float x)
+ { //Generalized smoothstep
+ x = Mathf.Clamp(x, 0.0f, 1.0f);
+  float result = 0;
+  for (var n = 0; n <= a - 1; n++) {
+    result += (pascalTriangle(-a, n) * pascalTriangle(2 * a - 1, a - n - 1) * Mathf.Pow(x, a + n));
+  }
+  return result;
+}
+
+int pascalTriangle(int a, int b){
+  float result = 1; 
+
+  for(float i = 1; i <= b; i++){
+      
+    result *= ((a - (i - 1)) / i);
+
+    
+  }
+
+  return Mathf.FloorToInt(result);
+}
+
+void Start()
+{
+    Debug.Log(pascalTriangle(3,3));
+}
+
+    Vector3 GetVertexPos( float theta, float phi)
+    {
+				float radius = GetRadius(eyeballRadius, theta);
+                float h = Mathf.Cos(theta);
+                float r = Mathf.Sin(theta)*radius;
+                
+                Vector3 p = new Vector3(Mathf.Cos(phi)*r,Mathf.Sin(phi)*r, h*radius);
+                return p;
+    }
+    // Start is called before the first frame update
+    void Update()
+    {
+        Mesh mesh;
+        mesh = new Mesh();
+        List<Vector3> vertices = new List<Vector3>();   
+        List<Vector3> normals = new List<Vector3>();
+
+        int a = 30;
+        int b = 30;
+        for ( int i = 0; i <=a; i++)
+        {
+            for (int j = 0; j < b; j++)
+            {
+                float progress = i/(float)(a);
+                float theta = Mathf.Pow(progress, verticesDensity)*Mathf.PI;
+                float phi = j/(float)(b)*Mathf.PI*2;
+                Vector3 p = GetVertexPos(theta, phi);
+                vertices.Add(p);
+                float delta = 0.01f;
+                Vector3 p1 = GetVertexPos(theta+delta, phi)-p;
+                //Vector3 p2 = GetVertexPos(theta-0.01f, phi);
+                Vector3 p3 = GetVertexPos(theta, phi+delta)-p;
+                float p3mag = p3.magnitude;
+                Vector3 n = Vector3.Cross(p1.normalized,p3.normalized ).normalized;
+                //Vector3 p4 = GetVertexPos(theta, phi-0.01f);
+                if (p3mag<= float.Epsilon )
+                {
+                    if ( i == 0 )
+                        n = new Vector3(0,0,1);
+                    else
+                        n = new Vector3(0,0,-1);
+                }
+                if( n.magnitude <= 0.0f)
+                    Debug.DebugBreak();
+                normals.Add(n);
+            }
+        }
+        mesh.vertices = vertices.ToArray();
+        List<int> indices = new List<int>();
+        
+        for ( int i = 0 ; i < a; i++)
+        for ( int j = 0; j < b; j++)
+        {
+            int r = i*a;
+
+            int q1 = (r+j);
+            int q2 = q1 + 1;
+            int q3 = (r+a+j);
+            int q4 = q3 + 1;
+
+            if (q2 >= (i+1)*a )
+                q2 = i*a;
+            if (q4 >= (i+2)*a )
+                q4 = (i+1)*a;                
+            indices.Add(q3);
+            indices.Add(q2);
+            indices.Add(q1);
+
+            indices.Add(q4);
+            indices.Add(q2);
+            indices.Add(q3);
+        }
+        mesh.normals = normals.ToArray();
+        mesh.triangles =  indices.ToArray();
+
+        GetComponent<MeshFilter>().mesh = mesh;
+
+        
+    }
+
+    // Update is called once per frame
+
+}
---
+++ Assets/NewUnlitShader.shader
@@ -0,0 +1,113 @@
+﻿Shader "Unlit/NewUnlitShader"
+{
+	Properties
+	{
+		_MainTex ("Texture", 2D) = "white" {}
+		_IrisPos("iris pos", range(-2,2)) = 0
+		_IrisSize("iris size", range(0.1,2)) = 0
+		_IrisUVScale("iris uv scale", range(0.0,10)) = 0
+		_IOR("ior", range(1.01,3))= 1
+		_PuipleSize("puile size", range(0.1,3))= 1
+	}
+	SubShader
+	{
+		Tags { "RenderType"="Opaque" }
+		LOD 100
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+			// make fog work
+			#pragma multi_compile_fog
+			
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+				float2 uv : TEXCOORD0;
+				float3 normal:NORMAL;
+			};
+
+			struct v2f
+			{
+				float2 uv : TEXCOORD0;
+				UNITY_FOG_COORDS(1)
+				float4 vertex : SV_POSITION;
+				float3 normal:TEXCOORD2;
+				float3 localPos: TEXCOORD3;
+			};
+
+			sampler2D _MainTex;
+			float4 _MainTex_ST;
+			float _IrisPos;
+			float _IrisSize;
+			float _IrisUVScale;
+			float _IOR;
+			float _PuipleSize;
+			bool intersectPlane(float3 n, float3 p0, float3 l0, float3 l, out float t) 
+            { 
+                float denom = dot(-n, l); 
+                if (denom > 1e-6) { 
+                    float3 p0l0 = p0 - l0; 
+                    t = dot(p0l0, -n) / denom; 
+                    return (t >= 0); 
+                } 
+            
+                return false; 
+            } 
+			v2f vert (appdata v)
+			{
+				v2f o;
+				o.localPos = v.vertex;//mul(unity_ObjectToWorld, v.vertex);
+				o.vertex = UnityObjectToClipPos(v.vertex);
+				o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+				UNITY_TRANSFER_FOG(o,o.vertex);
+				o.normal = v.normal;
+				
+				
+				return o;
+			}
+			
+			fixed4 frag (v2f i) : SV_Target
+			{
+				float3 localCamPos = mul(unity_WorldToObject, float4(_WorldSpaceCameraPos.xyz,1));
+				float3 viewDir = normalize( i.localPos - localCamPos);
+				float t;
+				float n1 = 1.0;
+				float n2 = _IOR;
+				float r = n1/n2;
+				float c = dot(-i.normal, viewDir);
+				float3 V_refraction = r*viewDir + (r*c - sqrt(1-pow(r,2)*(1-pow(c,2))))*i.normal;
+				
+				if (intersectPlane( float3(0,0,1), float3(0,0,_IrisPos), i.localPos, V_refraction, t ))
+				{
+					float3 hitpos = (i.localPos  + t*V_refraction);
+					if (length(hitpos.xy) < _IrisSize)
+					{	
+						
+						hitpos *=0.5;
+						hitpos -=0.5;
+						float2 dir = normalize(hitpos+0.5);
+						float d = length(hitpos+0.5);
+						d*=saturate(d/_PuipleSize);
+						hitpos.xy = dir*d-0.5;
+						hitpos = (hitpos + 0.5) *_PuipleSize -0.5;
+						return tex2D(_MainTex, hitpos);
+						return float4((hitpos).xy,0,1)*10;
+					}
+				}
+				
+					
+				// sample the texture
+				fixed4 col = tex2D(_MainTex, i.uv);
+				// apply fog
+				UNITY_APPLY_FOG(i.fogCoord, col);
+				return (i.normal.xyzz+1) /2;
+			}
+			ENDCG
+		}
+	}
+}
